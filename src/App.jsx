058ef351750128ec43ae3231170d9c2d877df039
Alexandra <alexandra.blandon@gmail.com>
2025-11-12 11:54:45 -0500

    implmented constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import GifGrabber from "./components/GifGrabber";
 import "./App.css";
 
 const API_KEY = "nlR1KBPhlM4DpEfcmFwNgztGAmDofwlR";
 const BASE_URL = "https://api.giphy.com/v1/gifs/search"; // api endpoint
 
 function App() {
-  // initialize state as an empty array -'setgifs' to update it and holds the search result
+  // initializes state as an empty array -'setgifs' to update it and holds the search result
   const [gifs, setGifs] = useState([]); // array to store fetched gifs
 
-  // initialize state as empty string, holds the text the user type
+  // initializes state as empty string, holds the text the user type
   const [Search, setSearch] = useState(""); // stores curent input value
-  return <></>;
+
+  // initializes state to track the app api interaction state
+  const [status, setStatus] = useState("idle"); //starting idle - neutral state
+
+  // initializes state to hold error messages from the api call
+  const [error, setError] = useState(null); //start at null
+
+  return (
+    <div className="app">
+      <h1>The GIPHY GIF Searcher</h1>
+      <SearchBar
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        onSearch={fetchGifs}
+        isLoading={status === "loading"}
+      />
+      <GifGrabber gifs={gifs} status={status} error={error} />
+    </div>
+  );
 }
 
 export default App;
