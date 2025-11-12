72030313fbbe091a61b941d3a945c443371c1439
Alexandra <alexandra.blandon@gmail.com>
2025-11-12 11:51:59 -0500

    implemented states

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const API_KEY = "nlR1KBPhlM4DpEfcmFwNgztGAmDofwlR";
+const BASE_URL = "https://api.giphy.com/v1/gifs/search"; // api endpoint
 
 function App() {
-  const [count, setCount] = useState(0)
+  // initialize state as an empty array -'setgifs' to update it and holds the search result
+  const [gifs, setGifs] = useState([]); // array to store fetched gifs
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  // initialize state as empty string, holds the text the user type
+  const [Search, setSearch] = useState(""); // stores curent input value
+  return <></>;
 }
 
-export default App
+export default App;
